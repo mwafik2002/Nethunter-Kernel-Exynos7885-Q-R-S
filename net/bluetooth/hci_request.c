abcf5d5900874d22ea2c9a1f72c37f5fb45b8bb0
MrRob0-X <hanya8493@gmail.com>
2022-06-06 19:02:27 +0000

    Revert "Bluetooth: Use req_workqueue for explicit connect requests"
    This reverts commit 9dad349c38d1a9ecde11a0646af54bb199f0de58.

--- net/bluetooth/hci_request.c
+++ net/bluetooth/hci_request.c
@@ -659,21 +659,8 @@ static void bg_scan_update(struct work_struct *work)
 {
 	struct hci_dev *hdev = container_of(work, struct hci_dev,
 					    bg_scan_update);
-	struct hci_conn *conn;
-	u8 status;
-	int err;
 
-	err = hci_req_sync(hdev, update_bg_scan, 0, HCI_CMD_TIMEOUT, &status);
-	if (!err)
-		return;
-
-	hci_dev_lock(hdev);
-
-	conn = hci_conn_hash_lookup_state(hdev, LE_LINK, BT_CONNECT);
-	if (conn)
-		hci_le_conn_failed(conn, status);
-
-	hci_dev_unlock(hdev);
+	hci_req_sync(hdev, update_bg_scan, 0, HCI_CMD_TIMEOUT);
 }
 
 void hci_request_setup(struct hci_dev *hdev)
