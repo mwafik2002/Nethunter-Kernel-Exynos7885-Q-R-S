fbc01b9fb7340bcd434b0e9ee589acd6b49188d3
MrRob0-X <hanya8493@gmail.com>
2022-06-06 18:58:48 +0000

    Revert "Bluetooth: Pass inquiry length to bredr_inquiry()"
    This reverts commit f1b4004bad367aab7977ca02f648388a4abbab39.

--- net/bluetooth/hci_request.c
+++ net/bluetooth/hci_request.c
@@ -853,7 +853,6 @@ static void le_scan_restart_work(struct work_struct *work)
 
 static int bredr_inquiry(struct hci_request *req, unsigned long opt)
 {
-	u8 length = opt;
 	struct hci_cp_inquiry cp;
 	/* General inquiry access code (GIAC) */
 	u8 lap[3] = { 0x33, 0x8b, 0x9e };
@@ -866,7 +865,7 @@ static int bredr_inquiry(struct hci_request *req, unsigned long opt)
 
 	memset(&cp, 0, sizeof(cp));
 	memcpy(&cp.lap, lap, sizeof(cp.lap));
-	cp.length = length;
+	cp.length = DISCOV_BREDR_INQUIRY_LEN;
 
 	hci_req_add(req, HCI_OP_INQUIRY, sizeof(cp), &cp);
 
@@ -960,7 +959,7 @@ static int interleaved_discov(struct hci_request *req, unsigned long opt)
 	if (err)
 		return err;
 
-	return bredr_inquiry(req, DISCOV_BREDR_INQUIRY_LEN);
+	return bredr_inquiry(req, opt);
 }
 
 static void start_discovery(struct hci_dev *hdev, u8 *status)
@@ -972,8 +971,7 @@ static void start_discovery(struct hci_dev *hdev, u8 *status)
 	switch (hdev->discovery.type) {
 	case DISCOV_TYPE_BREDR:
 		if (!hci_dev_test_flag(hdev, HCI_INQUIRY))
-			hci_req_sync(hdev, bredr_inquiry,
-				     DISCOV_BREDR_INQUIRY_LEN, HCI_CMD_TIMEOUT,
+			hci_req_sync(hdev, bredr_inquiry, 0, HCI_CMD_TIMEOUT,
 				     status);
 		return;
 	case DISCOV_TYPE_INTERLEAVED:
